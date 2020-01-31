b2c4811ffaf032bbd332eaff46e0c15993c5c3c5
DATBOI <alexsimons9999@gmail.com>
2020-01-30 19:02:09 -0600

    I will get this to work

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,25 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// Importthe module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('The Doki-Doki Theme has be activated');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "doki-theme" is now active!');
-
+	
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('extension.Satsuki', () => {
+		const otherthing = vscode.window.activeTextEditor;
+		const snipper = new vscode.SnippetString("yeet");
+		const workbench = vscode.extensions.getExtension('workbench.contributions.kind');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Yeet!');
+		console.log(Object.getPrototypeOf(vscode));
+		otherthing?.insertSnippet(snipper);
+		vscode.window.showInformationMessage('Satsuki Activated');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
