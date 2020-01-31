1a5e44b08aad7f026e577af50800789e2ebc79d7
DATBOI <alexsimons9999@gmail.com>
2020-01-31 17:20:27 -0600

    I do not need this

--- src/BackgroundService.ts
+++
@@ -1,5 +0,0 @@
-import {DokiTheme} from "./DokiTheme";
-
-export function installBackground(dokiTheme: DokiTheme) {
-
-}
--- src/ThemeManager.ts
+++ src/ThemeManager.ts
@@ -3,11 +3,9 @@ import {DokiTheme} from "./DokiTheme";
 import {installSticker} from "./StickerService";
 import {setLookAndFeel} from "./LaFService";
 import {activateIcons} from "./IconService";
-import {installBackground} from "./BackgroundService";
 
 export function activateTheme(dokiTheme: DokiTheme) {
   installSticker(dokiTheme);
-  installBackground(dokiTheme);
   setLookAndFeel(dokiTheme);
   activateIcons(dokiTheme);
   vscode.window.showInformationMessage(`${dokiTheme.name} has been activated, please restart your IDE`);
