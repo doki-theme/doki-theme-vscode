ae6e76a326f716d687231aabbea9ff573df7df32
DATBOI <alexsimons9999@gmail.com>
2020-02-02 12:18:32 -0600

    Can walk, just need the talk

--- buildSrc/BuildThemes.js
+++ buildSrc/BuildThemes.js
@@ -10,20 +10,26 @@ const definitionDirectoryPath =
 const templateDirectoryPath =
     path.resolve(repoDirectory, 'themes', 'templates');
 
-function walkDir(dir, callback) {
-    fs.readdirSync(dir).forEach(f => {
-        const dirPath = path.join(dir, f);
-        const isDirectory = fs.statSync(dirPath).isDirectory();
-        isDirectory ?
-            walkDir(dirPath, callback) : callback(path.join(dir, f));
-    });
+function walkDir(dir) {
+    const allPromises = Promise.all(fs.readdirSync(dir)
+        .map(file => {
+            const dirPath = path.join(dir, file);
+            const isDirectory = fs.statSync(dirPath).isDirectory();
+            if (isDirectory) {
+                return walkDir(dirPath)
+            } else {
+                return Promise.resolve(path.join(dir, file));
+            }
+        })).then(scannedDirectories => scannedDirectories
+            .reduce((accum, files) => accum.concat(files), []));
+    return allPromises;
 };
 
 // todo: get templates
 
 function getThemeType(dokiThemeTemplateJson) {
-    return dokiThemeTemplateJson.dark ? 
-    "dark": "light"
+    return dokiThemeTemplateJson.dark ?
+        "dark" : "light"
 }
 
 function buildLAFColors(dokiThemeTemplateJson) {
@@ -34,15 +40,78 @@ function buildSyntaxColors(dokiThemeTemplateJson) {
     return {};
 }
 
-walkDir(definitionDirectoryPath, (filePath) => {
-    if (filePath.endsWith('doki.json')) {
-        const dokiThemeTemplateJson =
-            JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-        const vsCodeTheme = {
-            type: getThemeType(dokiThemeTemplateJson),
-            colors: buildLAFColors(dokiThemeTemplateJson),
-            tokenColors: buildSyntaxColors(dokiThemeTemplateJson),
-        }
-        console.log(vsCodeTheme);
+function buildVSCodeTheme(
+    dokiThemeDefinition,
+    dokiTemplateDefinitions
+) {
+    return {
+        type: getThemeType(dokiThemeDefinition),
+        colors: buildLAFColors(
+            dokiThemeDefinition,
+            dokiTemplateDefinitions
+        ),
+        tokenColors: buildSyntaxColors(
+            dokiThemeDefinition,
+            dokiTemplateDefinitions
+        )
     }
-})
+}
+
+function createDokiTheme(
+    dokiFileDefinitonPath,
+    dokiTemplateDefinitions
+) {
+    const dokiThemeDefinition = 
+        readJson(dokiFileDefinitonPath);
+    const dokiTheme = {
+        definition: dokiThemeDefinition,
+        theme: buildVSCodeTheme(
+            dokiThemeDefinition, 
+            dokiTemplateDefinitions
+        )
+    }
+    return dokiTheme
+}
+
+const readJson = (jsonPath) =>
+    JSON.parse(fs.readFileSync(jsonPath, 'utf-8'));
+
+const readTemplates = templatePaths => {
+    return templatePaths
+        .map(readJson)
+        .reduce((accum, template) => {
+            accum[template.name] = template;
+            return accum;
+        }, {});
+};
+
+walkDir(templateDirectoryPath)
+    .then(readTemplates)
+    .then(dokiTemplateDefinitions => {
+        return walkDir(definitionDirectoryPath)
+            .then(files => files.filter(file => file.endsWith('doki.json')))
+            .then(dokiFileDefinitionPaths => {
+                return {
+                    dokiTemplateDefinitions,
+                    dokiFileDefinitionPaths
+                }
+            })
+    })
+    .then(templatesAndDefinitions => {
+        const {
+            dokiTemplateDefinitions,
+            dokiFileDefinitionPaths
+        } = templatesAndDefinitions;
+        return dokiFileDefinitionPaths.map(
+                dokiFileDefinitonPath =>
+                    createDokiTheme(
+                        dokiFileDefinitonPath,
+                        dokiTemplateDefinitions
+                    )
+            )
+    }).then(dokiThemes => {
+        // write to package json
+        // write things for extension
+        // copy to out directory
+        console.log(dokiThemes)
+    })
