cf052ce9d3aa24b169c0a6cca5f0c85da7722413
DATBOI <alexsimons9999@gmail.com>
2020-02-02 09:38:44 -0600

    Fleshing out the non-named colors

--- buildSrc/BuildThemes.js
+++ buildSrc/BuildThemes.js
@@ -3,6 +3,44 @@ const path = require('path');
 const currentDirectory = require.main.path;
 const repoDirectory = path.resolve(currentDirectory, '..');
 
+const satsukiThemePath = path.resolve(repoDirectory, 'src','themes','Satsuki.theme.json')
+
+const fs = require('fs');
+const satsukiJson = JSON.parse(fs.readFileSync(satsukiThemePath, 'utf-8'))
+
+const satsukiDefinitionPath = path.resolve(repoDirectory,'themes','definitions','killLaKill','satsuki','satsuki.doki.json')
+const satsukiDefinition = JSON.parse(fs.readFileSync(satsukiDefinitionPath, 'utf-8'));
+const satuskiColors = satsukiDefinition.colors;
+
+const defColorToName = Object.keys(satuskiColors)
+.map(k => ({key: k, value: satuskiColors[k]}))
+.reduce((acc, k) => {
+    const key = k.key;
+    const value = k.value.toUpperCase();
+    if(!acc[value]){
+        acc[value] = []
+    }
+
+    acc[value].push(key)
+    return acc;
+}, {});
+
+const templated = Object.keys(satsukiJson.colors)
+.map(key => ({key, value: satsukiJson.colors[key]}))
+.map(kV => {
+    const hexColor = kV.value.toUpperCase();
+    const mappedValue = 
+    defColorToName[hexColor.substring(0, 7)] ||
+    defColorToName[hexColor]
+    if(!mappedValue) {
+        throw new Error(`Unable to find named color for ${hexColor} for property ${kV.key}`)
+    }
+    const namedColor = mappedValue[0]
+
+    const templatedValue = `&${namedColor}&${hexColor.substring(7) || ''}`
+
+    return {key: kV.key, value: templatedValue}
+})
 
 
-console.log(repoDirectory);
+console.log(templated)
