1867aa21c44e1a4c4402916df04ef648958f38cb
DATBOI <alexsimons9999@gmail.com>
2020-04-18 06:20:34 -0500

    Added the framework to compare checksums

--- src/StickerUpdateService.ts
+++ src/StickerUpdateService.ts
@@ -1,14 +1,31 @@
 import * as vscode from 'vscode';
 import { getCurrentTheme } from './ThemeManager';
 import { performGet } from './RESTClient';
+import { installSticker } from './StickerService';
+import { DokiTheme } from './DokiTheme';
 
-export const attemptToUpdateSticker = (context: vscode.ExtensionContext) => {
+const fetchRemoteChecksum = async (currentTheme: DokiTheme) => {
+  const checkSumInputStream = await performGet('https://doki.assets.acari.io/stickers/vscode/reZero/rem/rem.png.checksum.txt');
+  return checkSumInputStream.setEncoding('utf8').read();
+};
+
+const fetchLocalChecksum = async (currentTheme: DokiTheme) => {
+  return 'yeet!';  
+};
+
+export const attemptToUpdateSticker = async (context: vscode.ExtensionContext) => {
   const currentTheme = getCurrentTheme();
   console.log(currentTheme.name, context.globalStoragePath);
-  performGet('https://doki.assets.acari.io/stickers/vscode/reZero/rem/rem.png.checksum.txt').then(result => {
-    console.log('I got this thing', result.setEncoding('utf8').read());
-  }).catch(er => {
-    console.log("Oh shit this", er);
-    
-  });
+
+  try {
+    const remoteChecksum = await fetchRemoteChecksum(currentTheme);
+    const localChecksum = await fetchLocalChecksum(currentTheme);
+    if(remoteChecksum !== localChecksum) {
+      console.log('sticker is different');
+      
+      // installSticker(currentTheme, context);
+    }    
+  } catch(e) {
+    console.error('Unable to check for updates', e);
+  }
 };
