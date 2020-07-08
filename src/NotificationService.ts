0eaea1713dc03f45c58b190a3ca8ec2b7922346e
DATBOI <alexsimons9999@gmail.com>
2020-07-08 04:22:20 -0500

    Added show changelog button

--- src/NotificationService.ts
+++ src/NotificationService.ts
@@ -1,17 +1,27 @@
-import * as vscode from 'vscode';
-import { VSCodeGlobals } from './VSCodeGlobals';
-import { attemptToGreetUser } from './WelcomeService';
+import * as vscode from "vscode";
+import { VSCodeGlobals } from "./VSCodeGlobals";
+import { attemptToGreetUser } from "./WelcomeService";
 
-const SAVED_VERSION = 'doki.theme.version';
-const DOKI_THEME_VERSION = 'v3.1.0';
+const SAVED_VERSION = "doki.theme.version";
+const DOKI_THEME_VERSION = "v3.2.0";
 
 export function attemptToNotifyUpdates(context: vscode.ExtensionContext) {
-    const savedVersion = VSCodeGlobals.globalState.get(SAVED_VERSION);
-    if (savedVersion && savedVersion !== DOKI_THEME_VERSION) {
-        VSCodeGlobals.globalState.update(SAVED_VERSION, DOKI_THEME_VERSION);
-        vscode.window.showInformationMessage(`Doki Theme updated to ${DOKI_THEME_VERSION}. Use "Doki Theme Changelog" command for more info.`);
-    } else if (!savedVersion) {
-        VSCodeGlobals.globalState.update(SAVED_VERSION, DOKI_THEME_VERSION);
-        attemptToGreetUser(context);
-    }
+  const savedVersion = VSCodeGlobals.globalState.get(SAVED_VERSION);
+  if (savedVersion && savedVersion !== DOKI_THEME_VERSION) {
+    VSCodeGlobals.globalState.update(SAVED_VERSION, DOKI_THEME_VERSION);
+    vscode.window
+      .showInformationMessage(
+        `Doki Theme updated to ${DOKI_THEME_VERSION}. 
+            Use "Doki Theme Changelog" command for more info.`,
+        { title: "Show Changelog" }
+      )
+      .then((item) => {
+        if (item) {
+          vscode.commands.executeCommand("extension.doki.changelog");
+        }
+      });
+  } else if (!savedVersion) {
+    VSCodeGlobals.globalState.update(SAVED_VERSION, DOKI_THEME_VERSION);
+    attemptToGreetUser(context);
+  }
 }
