2aae123179555f87edc21521581a2078853aa2e4
DATBOI <alexsimons9999@gmail.com>
2021-04-27 17:51:30 -0500

    Updated changelog and bumped version to v11.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+# 11.1.2 [Network Error Handling]
+
+- Alerting user that asset installation has failed because of network issues.
+
 # 11.1.1 [Better Glass Pane Experience]
 
 - Fixed annoying background dragging in file tree. Be sure to re-install the wallpaper for this to take effect.
--- src/NotificationService.ts
+++ src/NotificationService.ts
@@ -3,7 +3,7 @@ import { VSCodeGlobals } from "./VSCodeGlobals";
 import { attemptToGreetUser } from "./WelcomeService";
 
 const SAVED_VERSION = "doki.theme.version";
-const DOKI_THEME_VERSION = "v11.1.1";
+const DOKI_THEME_VERSION = "v11.1.2";
 
 export function attemptToNotifyUpdates(context: vscode.ExtensionContext) {
   const savedVersion = VSCodeGlobals.globalState.get(SAVED_VERSION);
